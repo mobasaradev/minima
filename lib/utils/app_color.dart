75d20eacdb86b7ea3544353dcd91e71bfef8b47a
dev-Sara <mkmobasara.dev@gmail.com>
2022-12-25 21:07:51 +0600

    add utils app_color

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class NoteColor {
+  NoteColor._();
+  static const primaryColor = Color(0xFFFD8A8A);
+  static const secondaryColor = Color(0xFFF1F7B5);
+  static const skyColor = Color(0xFFA8D1D1);
+  static const purpleColor = Color(0xFF9EA1D4);
+}
