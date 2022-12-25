66ded0e7e14302f748e9b87a7fe26f8b6938e2ab
dev-Sara <mkmobasara.dev@gmail.com>
2022-12-25 20:22:58 +0600

    initial commit

---
+++ lib/Note_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Notes App',
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:counter_app/Note_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
