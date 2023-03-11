ea4840d02b055a72719381f76f834376650c3e2b
dev-Sara <mkmobasara.dev@gmail.com>
2023-03-11 20:57:31 +0600

    update local datebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:counter_app/Note_app.dart';
 import 'package:counter_app/modules/home/cubit/note/note_cubit.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
   runApp(
     BlocProvider<NoteCubit>(
       create: (context) => NoteCubit(),
